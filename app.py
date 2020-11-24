fd5fac423432f6742c9357afa99adf51e5b02ac7
ThatScode24 <65173747+ThatScode24@users.noreply.github.com>
2020-11-24 11:30:02 +0200

    Bot is now running

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-import discord
-import time
+__author__ = 'miha_focsa'
 
-emm = True
+import discord 
+from discord.ext import commands
 
-TOKEN = ''
+
+TOKEN = 'NzgwNzI0NjE0Nzk5NTU2NjA4.X7zQdQ.plWb_Cp_KBsH4NZgmLm37Y0wx7w'
+PREFIX = 'stp '
+
+bot = commands.Bot(command_prefix=PREFIX)
+
+@bot.event
+async def on_ready():
+	print("Logged in as: " + bot.user.name + "\n")
+	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Saft"))
+
+
+
+if __name__ == '__main__':
+	bot.run(TOKEN)
